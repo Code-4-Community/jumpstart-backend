307deeb9e10a810a5753d209587d38ff78425123
Conner Nilsen <nilsen.c@husky.neu.edu>
2020-07-18 13:29:02 -0400

    Add detailed comments to pom and classes to better describe what's going on

--- api/src/main/java/com/codeforcommunity/ApiMain.java
+++ api/src/main/java/com/codeforcommunity/ApiMain.java
@@ -10,16 +10,29 @@ import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.CorsHandler;
 import java.util.Set;
 
+/**
+ * Our 'ServiceMain' for initializing the API server. Sets up routing and actually sets the server
+ * to listen on some port (initially 8089).
+ */
 public class ApiMain {
   public ApiMain() {}
 
   /** The initialize the sub-router and start the API server. */
   public void startApi() {
+    // Get a 'Vertx' object. This will provide us an HttpServer and Router.
     Vertx vertx = Vertx.vertx();
+    // Get an HttpServer from the Vertx object.
     HttpServer server = vertx.createHttpServer();
 
+    // Create a new router using the vertx object.
     Router router = Router.router(vertx);
-    // Set up cors so that back end and front end can interact through different servers
+    /*
+     * Set up CORS so that back end and front end can interact through different servers. Usually
+     * web browsers will only allow a front end to fetch resources from back-ends
+     * that have the same address as them (with a few caveats), but this allows us to 'allow' other
+     * things too.
+     * See https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS for more info.
+     */
     router
         .route()
         .handler(
@@ -37,9 +50,21 @@ public class ApiMain {
 
     // Create a route for the home (root) path
     Route home = router.route("/");
+    /*
+     * Set the handler method for the home path. Whenever the home path is loaded, the 'handleHome'
+     * function is called.
+     *
+     * The "this::handleHome" style is called a method reference, and it is a type of lambda in
+     * Java. For more info on lambdas in Java check out the following:
+     * - Lambdas https://www.tutorialspoint.com/java8/java8_lambda_expressions.htm
+     * - Method References https://www.tutorialspoint.com/java8/java8_method_references.htm
+     * - Functional Interfaces https://www.tutorialspoint.com/java8/java8_functional_interfaces.htm
+     */
+
     home.handler(this::handleHome);
 
     // Start the server and listen on port :8081
+    // (you can access this locally at http://localhost:8081)
     server.requestHandler(router).listen(8081);
   }
 
@@ -56,14 +81,14 @@ public class ApiMain {
             .setStatusCode(200)
             // Sets the 'Content-Type' of this response
             .putHeader("Content-Type", "text/plain")
-            // Sets the CORS values set up above for the response
+            // Sets the CORS to the same thing we did above above for the response
             .putHeader("Access-Control-Allow-Origin", "*")
             .putHeader("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS")
             .putHeader(
                 "Access-Control-Allow-Headers",
                 "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-    // Returns 'Hello World' as a response
-    response.end("Hello World");
+    // Returns 'Hello Jumpstarters!' as a response
+    response.end("Hello Jumpstarters!");
   }
 }
--- service/src/main/java/com/codeforcommunity/ServiceMain.java
+++ service/src/main/java/com/codeforcommunity/ServiceMain.java
@@ -1,12 +1,27 @@
 package com.codeforcommunity;
 
-/** The main class for this application. Sets up and starts the API server. */
+/**
+ * The main class for this application. Sets up and starts the API server, and is our entry point
+ * into the program.
+ */
 public class ServiceMain {
+
+  /**
+   * The main method. What gets called during the startup of this app.
+   *
+   * @param args A list of strings that gets passed through the command line when you call the app.
+   *     For example, if the app were started like {@code java -jar myapp.jar --port=5000}, it would
+   *     give you something like {@code ["--port=5000"]}.
+   */
   public static void main(String[] args) {
+    // Try to run our program, and catch any exception that isn't caught before this.
     try {
+      // Instantiate a ServiceMain object.
       ServiceMain serviceMain = new ServiceMain();
+      // Call the initialize method of that object.
       serviceMain.initialize();
     } catch (Exception e) {
+      // Print the stack trace of the exception, and then exit the program.
       e.printStackTrace();
     }
   }
